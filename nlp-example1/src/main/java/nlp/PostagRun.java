8f7642a440ae50e4ef539b4f9f87bcf7e57e6a48
Uchirgerel <44520693+Uchirgerel@users.noreply.github.com>
2020-02-24 13:21:30 +0800

    Add files via upload

---
+++ nlp-example1/src/main/java/nlp/PostagRun.java
@@ -0,0 +1,33 @@
+package nlp;
+
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class PostagRun {
+    public static void main(String[] args)
+    {
+        Logger logger = LoggerFactory.getLogger(PostagRun.class);
+
+        StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
+
+        String txt = "These young guys are playing checkers. I'm out there playing chess";
+        CoreDocument doc = new CoreDocument(txt);
+
+        stanfordCoreNLP.annotate(doc);
+
+        List<CoreLabel> lblList = doc.tokens();
+        for(CoreLabel lbl: lblList)
+        {
+            String pos = lbl.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            System.out.println(lbl.originalText() + " - " + pos);
+        }
+    }
+
+}
---
+++ nlp-example1/src/main/java/nlp/SentenceRun.java
@@ -0,0 +1,31 @@
+package nlp;
+
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreEntityMention;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.List;
+
+public class SentenceRun {
+    public static void main(String[] args)
+    {
+        Logger logger = LoggerFactory.getLogger(SentenceRun.class);
+        logger.info("Sentence logger started");
+
+        StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
+
+        String txt = "Hello! I am Uchirgerel. My lesson started.";
+
+        CoreDocument doc = new CoreDocument(txt);
+        stanfordCoreNLP.annotate(doc);
+
+        List<CoreSentence> sens = doc.sentences();
+        for (CoreSentence sen: sens)
+        {
+            System.out.println(sen.toString());
+        }
+    }
+}
